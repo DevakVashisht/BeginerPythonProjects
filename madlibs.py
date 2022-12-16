abeb1edab3241a4cf4cd39ceec1e307345cbd57c
Devak Vashisht <120743172+DevakVashisht@users.noreply.github.com>
2022-12-16 22:50:54 +0530

    Madlibs game

---
+++ madlibs.py
@@ -0,0 +1,8 @@
+My_Adjective = input("Adjective: ")
+My_Verb = input("Verb: ")
+My_Verb2 = input("write another verb: ")
+Favourite_person = input("Enter the name of your favourite person: ")
+
+madlib = f"Computer programming is so {My_Adjective}! it makes me so excited because i love to {My_Verb}\nStay Hydrated and {My_Verb2} like {Favourite_person}"
+
+print(madlib)
