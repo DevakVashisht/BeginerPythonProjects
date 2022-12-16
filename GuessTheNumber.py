3c7f86ad8414b1a5da16a134596a5b00f05c2f2a
Devak Vashisht <120743172+DevakVashisht@users.noreply.github.com>
2022-12-16 23:07:34 +0530

    Python projects :-)

---
+++ GuessTheNumber.py
@@ -0,0 +1,17 @@
+import random
+
+def fun_guess(x):
+    game_continue = True
+    Guess = random.randint(1, x)
+    while game_continue:
+        user_guess = int(input(f"Guess a number between 1 and {x}\n"))
+        if user_guess > Guess:
+            print(f"Guess a number less than {user_guess}")
+        elif user_guess < Guess:
+            print(f"Guess a number greater than {user_guess}")
+        elif user_guess == Guess:
+            print(f"The answer was {Guess}")
+            print("You won!")
+            game_continue = False
+            break
+fun_guess(10)
